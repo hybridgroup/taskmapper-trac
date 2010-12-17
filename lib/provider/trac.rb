3b651a90fe72633915d46e5e84d98960ac3cd4d4
Rafael George <george.rafael@gmail.com>
2010-12-17 20:06:33 +0000

    initial code with trac4r is still buggy

--- lib/provider/trac.rb
+++ lib/provider/trac.rb
@@ -4,11 +4,18 @@ module TicketMaster::Provider
     include TicketMaster::Provider::Base
     
     # This is for cases when you want to instantiate using TicketMaster::Provider::Yoursystem.new(auth)
-    def self.new(auth = {})
+    def self.new(url, auth = {})
+      @url = url
       TicketMaster.new(:trac, auth)
     end
-    
+
+    def authorize(auth = {})
+      @authentication ||= TicketMaster::Authenticator.new(auth)
+      auth = @authentication
+      @trac = Trac.new(@url, auth.login, auth.password)
+    end
+
     # declare needed overloaded methods here
-    
+
   end
 end
--- spec/projects_spec.rb
+++ spec/projects_spec.rb
@@ -1,5 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Ticketmaster::Provider::Trac::Project" do
-  it "should have specs for projects"
+
+  before(:all) do
+    @klass = TicketMaster::Provider::Trac::Project
+  end
+
+  before(:each) do
+    @trac = TicketMaster::Provider::Trac.new('http://trac.edgewall.org',{:login => 'cored', :password => 'jdudk'})
+  end
+
+  it "should be able to load all projects" do
+    projects = @trac.projects
+  end
 end
