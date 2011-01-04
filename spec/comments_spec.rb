dee8107f68e786884ab5d7e765ba9178fecf1f40
Rafael George <george.rafael@gmail.com>
2011-01-04 19:24:49 -0400

    added initial spec for comments

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -1,5 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Ticketmaster::Provider::Trac::Comment" do
-  it "should have specs for comments"
+  before(:each) do 
+    @ticketmaster = TicketMaster.new(:trac, {:username => 'george.rafael@gmail.com', :password => '123456', :url => 'http://pl3.projectlocker.com/cored/testrepo/trac'})
+    @project = @ticketmaster.projects.first
+    @ticket = @project.tickets.first
+    @klass = TicketMaster::Provider::Trac::Comment
+  end
+
+  it "should load all comments from a ticket" do 
+    @ticket.comments.should be_an_instance_of(Array)
+    @ticket.comments.first.should be_an_instance_of(@klass)
+  end
 end
