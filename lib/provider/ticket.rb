37794aa431984009ce3cae32d22ce707e9e6ac52
Rafael George <george.rafael@gmail.com>
2011-01-05 19:15:21 -0400

    added comments read functionality - tried to add attributes search of comments - fixed attributes search of comments - made a minimal refactoring for selecting comments using an array of id's - added the ability to search for a comment using an id - comment method in ticket should return an instance of comment - added the ability to search comments using attributes

--- lib/provider/ticket.rb
+++ lib/provider/ticket.rb
@@ -50,21 +50,32 @@ module TicketMaster::Provider
       end
 
       def comments(*options)
-        comments = CommentUtil.new(self.id).comments
-        if options.empty?
-          comments.collect do |comment|
-            TicketMaster::Provider::Trac::Comment.new comment
+        comments = CommentUtil.new(self.id).comments.collect { |comment| TicketMaster::Provider::Trac::Comment.new comment }
+        if options.first.is_a? Array
+          comments.select do |comment|
+            comment if options.first.any? { |id| id == comment.id }
           end
-        elsif options.first.is_a? Array
-          comments.collect do |comment|
-            TicketMaster::Provider::Trac::Comment.new comment if options.first.any? { |id| id == comment[:id] }
+        elsif options.first.is_a? Hash
+          hash = options.first
+          comments.select do |key, value|
+            hash[key] == value
           end
+        else
+          comments
         end
       end
 
-      def comment
-        warn "Trac doesn't support comments"
-        nil
+      def comment(*options)
+        comments = CommentUtil.new(self.id).comments.collect { |comment| TicketMaster::Provider::Trac::Comment.new comment }
+        if options.empty?
+          TicketMaster::Provider::Trac::Comment.new
+        elsif options.first.is_a? Fixnum
+          comments.select { |comment| comment[:id] == options.first }.first
+        elsif options.first.is_a? Hash
+          comments.select do |key, value| 
+            options.first[key] == value
+          end.first
+        end
       end
 
       def comment!
