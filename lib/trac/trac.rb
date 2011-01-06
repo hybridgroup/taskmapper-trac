1a23eebdb94d773dfb1ffde1f66fe2cad9ec019f
Rafael George <george.rafael@gmail.com>
2011-01-06 19:14:56 -0400

    added author field to comments

--- lib/trac/trac.rb
+++ lib/trac/trac.rb
@@ -35,20 +35,26 @@ class CommentUtil
     comment_id = 0
     @doc.css('h3.change a.timeline').collect do |value|
       body = @doc.css('div.comment p')
+      authors = @doc.css('h3.change')
       comment_id += 1
-      build_comment_hash(value, comment_id, body[comment_id-1].text)
+      build_comment_hash(value, comment_id, body[comment_id-1].text, authors[comment_id-1].content)
     end
   end
 
   private
-  def build_comment_hash(value, comment_id, body)
+  def build_comment_hash(value, comment_id, body, author)
     comment = {}
     comment[:created_at] = date_from_attributes(value)
     comment[:updated_at] = date_from_attributes(value)
     comment[:ticket_id] = @ticket_id
     comment[:body] = body
+    comment[:author] = normalize_author_field(author)
     comment[:id] = comment_id
     comment
+  end
+
+  def normalize_author_field(author)
+    author.strip.split(/by/)[1]
   end
 
   def date_from_attributes(value)
