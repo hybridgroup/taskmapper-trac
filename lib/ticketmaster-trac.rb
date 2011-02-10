56936145ef723939eac1d071b4813d8af288fd2d
Rafael George <george.rafael@gmail.com>
2011-02-09 20:47:34 -0400

    fix absolute path in trac api require

--- lib/ticketmaster-trac.rb
+++ lib/ticketmaster-trac.rb
@@ -3,7 +3,7 @@ require 'trac4r'
 require 'nokogiri'
 require 'open-uri'
 
-require 'lib/trac/trac'
+require File.dirname(__FILE__) + '/trac/trac'
 
 %w{ trac ticket project comment }.each do |f|
   require File.dirname(__FILE__) + '/provider/' + f + '.rb';
