a4cc726204b2e5fd52537bf656dc5021e393a84f
Rafael George <george.rafael@gmail.com>
2011-04-04 20:22:26 -0400

    added a pending spec for commentutil

--- lib/ticketmaster-trac.rb
+++ lib/ticketmaster-trac.rb
@@ -1,7 +1,11 @@
 require 'ticketmaster'
+require 'openssl'
 require 'trac4r'
 require 'nokogiri'
 require 'open-uri'
+
+# Hack for SSL problem 
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 require File.dirname(__FILE__) + '/trac/trac'
 
--- spec/commentutil_spec.rb
+++ spec/commentutil_spec.rb
@@ -2,4 +2,13 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "CommentUtil" do 
 
+  before(:each) do 
+    trac = {:url => 'https://pl3.projectlocker.com/CapLinked/Website/trac/', :username => 'ron@hybridgroup.com', :password => 'password'}
+    @comment_util = CommentUtil.new(1, trac)
+  end
+
+  it "should return all comments for a ticket" do 
+    pending("Fix me")
+    @comment_util.comments.should be_instance_of(Array)
+  end
 end
